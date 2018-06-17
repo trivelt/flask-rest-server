c9dc1792aa09127b643c28942ee1ffbd90486735
Maciej Michalec <maciej.michalec@gmail.com>
2018-06-17 15:22:59 +0200

    Changed max length of values in ip_address column

--- rest_server/app/models/client.py
+++ rest_server/app/models/client.py
@@ -1,11 +1,14 @@
 from app import db
 from app.models.dataset import Dataset
 
+MAX_NAME_LENGTH = 255
+MAX_IP_LENGTH = 45
+
 
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    ip_address = db.Column(db.String(80))
+    name = db.Column(db.String(MAX_NAME_LENGTH))
+    ip_address = db.Column(db.String(MAX_IP_LENGTH))
     datasets = db.relationship('Dataset', order_by=Dataset.id, back_populates='client')
 
     def __repr__(self):
--- rest_server/app/models/dataset.py
+++ rest_server/app/models/dataset.py
@@ -1,12 +1,14 @@
 from app import db
 import json
 
+MAX_FILENAME_LENGTH = 255
+
 
 class Dataset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
     client = db.relationship('Client', back_populates='datasets')
-    filename = db.Column(db.String(255))
+    filename = db.Column(db.String(MAX_FILENAME_LENGTH))
     userdata = db.Column(db.Text)
 
     def __repr__(self):
